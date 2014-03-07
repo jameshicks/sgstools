b953de6a8927fb5555ba53025e8c216715ad5b07
James Hicks <james@motorcyclesarejets.com>
2014-03-06 20:39:49 -0500

    major optimizations

--- share_markers_population_presample.py
+++ share_markers_population_presample.py
@@ -8,26 +8,23 @@ from multiprocessing import Pool
 
 import numpy as np 
 
-nrep=1000
-nthreads=10
+nrep = 1000
+
+parallel = True
+nthreads = 8
 
 
 def share(pair):
     retval = np.zeros(nmark)
     shares = shared[pair]
-    lshares = len(shares)
-    sidx = 0
-    start, stop = shares[sidx]
-    for i,p in enumerate(positions):
-        if p > stop:
-            sidx += 1
-            if (sidx + 1) > lshares:
-                break
-            start, stop = shares[sidx]
-        if start <= p <= stop:
-            retval[i] = 1
+
+    for start, stop in shares:
+        a,b = posd[start],posd[stop]
+        retval[a:(b+1)] = 1 
+
     return retval
-    
+
+
 def shares(inds):
     ninds = len(inds)
     tmaxshares = 0.5 * ninds * (ninds - 1)
@@ -39,17 +36,21 @@ def shares(inds):
         nshare += share(pair)
     return nshare / tmaxshares
 
-
+print 'Reading individual lists'
 with open(sys.argv[3]) as f:
     affinds = set('.'.join(x.strip().split()) for x in f)
     naff = len(affinds)
 with open(sys.argv[4]) as f:
     fullinds = set('.'.join(x.strip().split()) for x in f)
+
+print 'Reading map'
 with open(sys.argv[2]) as f:
     map = [x.strip().split() for x in f]
     positions = [int(x[3]) for x in map]
     nmark = len(positions)
-print 'reading shares'
+    posd =  dict([(y,x) for x,y in enumerate(positions)])
+
+print 'Reading Share file'
 with open(sys.argv[1]) as sharef:
     shared = {}
     for i,line in enumerate(sharef):
@@ -59,39 +60,40 @@ with open(sys.argv[1]) as sharef:
         ind1 = '.'.join(l[0:2])
         ind2 = '.'.join(l[2:4])
         pair = frozenset([ind1,ind2])
-        #if not pair <= affinds: continue
         start,stop = [int(x) for x in l[5:7]]
         if pair not in shared:
             shared[pair] = []
         shared[pair].append([start, stop])
-    print 'sorting'
-    for k in shared:
-        shared[k] = sorted(shared[k])
     keyset = frozenset(shared.keys())
 
-print 'calc affectedshares'
+print 'Calculating sharing from affecteds'
 affshare = shares(affinds)
 
-print 'calc nullshares'
 
+print 'Calculating sharing from %s draws of %s individuals' % (nrep, naff)
 def nsharehelper(x):
     print 'sharecall %s' % x 
     return shares(sample(fullinds, naff))
 
-pool = Pool(processes=nthreads)
+if parallel:
+    pool = Pool(processes=nthreads)
+    nullshares = pool.map(nsharehelper, xrange(nrep))
+else:
+    nullshares = map(nsharehelper, xrange(nrep))
 
-nullshares = pool.map(nsharehelper, xrange(nrep))
 nullshares = zip(*nullshares)
 
 
 
-print 'calc pvals'
+print 'Calculating Empirical P-values'
 pvals = [sum(1 for ns in n if ns >= a) / float(nrep)
          for a,n in izip(affshare, nullshares)]
 
-
+print 'Minimum observed P: %s' % min(pvals)
+print
+print 'Writing output'
 with open(sys.argv[5],'w') as f:
-    
+    f.write(','.join(['chr','snp','cm','pos','pctshares', 'p%s' % nrep]) + '\n')
     for m,a,p in izip(map, affshare, pvals):
         chr,snp,cm,pos = m
         f.write(','.join(str(x) for x in [chr,snp,cm,pos,a,p]))
