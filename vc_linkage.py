cb63aaa87eb86ce98c558b9ddc920bcf5980c3a8
James Hicks <james@motorcyclesarejets.com>
2015-08-13 17:32:00 -0500

    incremental progress

--- vc_linkage.py
+++ vc_linkage.py
@@ -7,7 +7,7 @@ import pydigree
 from pydigree.mixedmodel import MixedModel
 from pydigree.sgs import ibd_matrix
 
-# Parse Arguments 
+# Parse Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--ped')
 parser.add_argument('--map')
@@ -19,8 +19,10 @@ parser.add_argument('--every', help='Distance between evaluations')
 args = parser.parse_args()
 
 # Support functions
+
+
 def read_map(filename):
-	pass
+    pass
 
 # Read pedigree data
 peds = pydigree.io.read_ped(args.ped)
@@ -29,7 +31,8 @@ peds = pydigree.io.read_ped(args.ped)
 # Read phenotype data
 pydigree.io.read_phenotypes(peds, args.phen)
 # TODO: Get valid individuals from phenotypes
-analysis_individuals = [x for x in peds.individuals if args.outcome in x.phenotypes]
+analysis_individuals = [x for x in peds.individuals
+	if args.outcome in x.phenotypes]
 
 # TODO: Read SGS data
 
@@ -44,21 +47,24 @@ print 'Done'
 
 # TODO: Find evaluation sites
 def find_site(target, values):
-	pass
+    pass
+
+def vc_linkage(null_model, locus):
+	# TODO: Find evaluation site's locus
+    eval_site = None
+    ibd_model = null_model.copy()
+    ibdmat = sgs.ibd_matrix(sgs, individuals, locus, location_type='physical')
+    ibd_model.add_random_effect('IBD', analysis_individuals, ibdmat)
+
+    ibd_model.fit()
+    ibd_model.maximize()
+    return ibd_model
 
 for x in xrange(evaluation_sites):
-	# TODO: Find evaluation site's locus 
-	eval_site = None
-	ibd_model = null_model.copy()
-	ibdmat = ibd_matrix(sgs, individuals)
-	ibd_model.add_random_effect('IBD', analysis_individuals, eval_site)
+    
+	ibd_model = vc_linkage(null_model, x)
+    llik_ibd = ibd_model.restricted_loglikelihood()
 
-	ibd_model.fit()
-	ibd_model.maximize()
-	llik_ibd = ibd_mode.restricted_loglikelihood()
-
-	likelihood_ratio = -2*(llik_ibd - llik_null) 
-	lod = likelihood_ratio / 4.6
-	print '\t'.join([chrom, pos, likelihood_ratio, lod])
-
-
+    likelihood_ratio = -2*(llik_ibd - llik_null)
+    lod = likelihood_ratio / 4.6
+    print '\t'.join([chrom, pos, likelihood_ratio, lod])
