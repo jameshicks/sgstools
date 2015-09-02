33bb1a7d2a35341cec722171055534816fab0230
James Hicks <james@motorcyclesarejets.com>
2015-09-02 17:34:49 -0500

    incremental updates

--- vc_linkage.py
+++ vc_linkage.py
@@ -3,11 +3,13 @@ import argparse
 from bisect import bisect_left
 from math import log, log10, e
 
+import numpy as np
+
 import pydigree
 
 from pydigree.common import log_base_change
 from pydigree.mixedmodel import MixedModel
-from pydigree.sgs import ibd_matrix
+
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
@@ -17,28 +19,27 @@ parser.add_argument('--phen')
 parser.add_argument('--sgs')
 parser.add_argument('--outcome')
 parser.add_argument('--fixefs')
-parser.add_argument('--every', help='Distance between evaluations')
+parser.add_argument('--every', default=0.5, type=float, 
+                    help='Distance between evaluations (in Mb)')
 args = parser.parse_args()
-
-# Support functions
-
-
-def read_map(filename):
-    pass
 
 
 # Read pedigree data
 peds = pydigree.io.read_ped(args.ped)
-# TODO: Read genotype map data
+
+# Read genotype map data
+chroms = pydigree.io.plink.read_map(args.map)
+for chrom in chroms:
+    peds.add_chromosome(chrom)
 
 # Read phenotype data
 pydigree.io.read_phenotypes(peds, args.phen)
 
 # Get valid individuals from phenotypes
 analysis_individuals = [x for x in peds.individuals
-	if args.outcome in x.phenotypes]
+                        if args.outcome in x.phenotypes]
 
-# TODO: Read SGS data
+# Read SGS data
 sgs = pydigree.io.sgs.read_germline(args.sgs)
 sgs.update_segment_references(peds)
 
@@ -47,18 +48,11 @@ null_model = MixedModel(peds, outcome=args.outcome, fixefs=args.fixefs)
 null_model.add_genetic_effect()
 null_model.fit_model()
 null_model.maximize()
-llik_null = null_model.restricted_loglikelihood()
+llik_null = null_model.full_loglikelihood()
 print 'Done'
 
 
-# TODO: Find evaluation sites
-def find_site(target, values):
-    pass
-
-
 def vc_linkage(null_model, locus):
-	# TODO: Find evaluation site's locus
-    eval_site = None
     ibd_model = null_model.copy()
     ibdmat = sgs.ibd_matrix(sgs, individuals, locus, location_type='physical')
     ibd_model.add_random_effect('IBD', analysis_individuals, ibdmat)
@@ -67,11 +61,17 @@ def vc_linkage(null_model, locus):
     ibd_model.maximize()
     return ibd_model
 
-for x in xrange(evaluation_sites):
+for chromidx, chromosome in enumerate(peds.chromosomes):
+    pstart, pstop = chromosome.physical_map[0], chromosome.physical_map[-1]
+    evaluation_sites = np.arange(pstart, pstop, args.every)
 
-	ibd_model = vc_linkage(null_model, x)
-    llik_ibd = ibd_model.restricted_loglikelihood()
+    for evaluation_site in evaluation_sites:
+        markidx = chromosome.closest_marker(evaluation_site)
+        locus = chromidx, markidx
+        ibd_model = vc_linkage(null_model, locus)
+        llik_ibd = ibd_model.full_loglikelihood()
 
-    lod = (log_base_change(llik_ibd, e, 10) - log_base_change(llik_null, e, 10)
-    
-    print '\t'.join([chrom, pos, lod])
+        lod = (log_base_change(llik_ibd, e, 10) -
+               log_base_change(llik_null, e, 10))
+
+        print '\t'.join([chromosome.label, evaluation_site, lod])
