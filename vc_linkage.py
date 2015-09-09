674604e634e0c0d9a8fae81eeea0b3ac18c70478
James Hicks <james@motorcyclesarejets.com>
2015-09-09 18:04:30 -0500

    incremental updates

--- vc_linkage.py
+++ vc_linkage.py
@@ -3,6 +3,7 @@ import argparse
 from math import log, log10, e
 
 import numpy as np
+from scipy.stats import chi2
 
 import pydigree
 
@@ -21,6 +22,10 @@ parser.add_argument('--outcome', required=True)
 parser.add_argument('--fixefs')
 parser.add_argument('--every', default=0.5, type=float,
                     help='Distance between evaluations (in Mb)')
+parser.add_argument('--onlywithin', action='store_true')
+parser.add_argument('--sites', nargs='*', type=int, dest='evalsites')
+parser.add_argument('--method', default='FS', dest='maxmethod')
+parser.add_argument('--verbose', action='store_true', help='Show progress of maximizer')
 args = parser.parse_args()
 
 
@@ -49,20 +54,23 @@ sgs = read_germline(args.sgs)
 print 'Updating references'
 sgs.update_segment_references(peds)
 
+
+
 print 'Fitting polygenic model'
 null_model = MixedModel(peds, outcome=args.outcome, fixed_effects=args.fixefs)
 null_model.add_genetic_effect()
 null_model.fit_model()
-null_model.maximize()
+null_model.maximize(method=args.maxmethod, verbose=args.verbose)
 null_model.summary()
 llik_null = null_model.loglikelihood()
 print 'Done'
 
 
-def vc_linkage(null_model, locus):
+def vc_linkage(locus):
     ibd_model = MixedModel(peds, outcome=args.outcome, fixed_effects=args.fixefs)
     ibd_model.add_genetic_effect()
-    ibdmat = sgs.ibd_matrix(analysis_individuals, locus, location_type='index')
+    ibdmat = sgs.ibd_matrix(analysis_individuals, locus, location_type='index', onlywithin=args.onlywithin)
+
     ranef = RandomEffect(analysis_individuals,
                          'IBD',
                          incidence_matrix='eye',
@@ -70,22 +78,48 @@ def vc_linkage(null_model, locus):
     ibd_model.add_random_effect(ranef)
 
     ibd_model.fit_model()
-    ibd_model.maximize()
+    ibd_model.maximize(verbose=args.verbose, method=args.maxmethod)
     return ibd_model
 
+class VCLResult(object):
+    def __init__(self, alternative, null):
+        self.llik_alt = alternative.loglikelihood()
+        self.llik_null = null.loglikelihood()
+
+    @property
+    def chisq(self):
+        return -2.0 * self.llik_null + 2.0 * self.llik_alt 
+    
+    @property
+    def pvalue(self):
+        return 1 - chi2.cdf(self.chisq, 1)
+    
+    @property
+    def lod(self):
+        return self.chisq / (2.0 * log(10.0))
+
+outputlist = []
 for chromidx, chromosome in enumerate(peds.chromosomes):
     pstart, pstop = chromosome.physical_map[0], chromosome.physical_map[-1]
-    evaluation_sites = np.arange(pstart, pstop, args.every * 1e6)
-
+    if not args.evalsites:
+        evaluation_sites = np.arange(pstart, pstop, args.every * 1e6)
+    else:
+        evaluation_sites = args.evalsites
     for evaluation_site in evaluation_sites:
         markidx = chromosome.closest_marker(evaluation_site)
         locus = chromidx, markidx
-        ibd_model = vc_linkage(null_model, locus)
+        
+        ibd_model = vc_linkage(locus)
         llik_ibd = ibd_model.loglikelihood()
-
+        vc = VCLResult(ibd_model, null_model)
         lod = (log_base_change(llik_ibd, e, 10) -
                log_base_change(llik_null, e, 10))
-
-        import ipdb; ipdb.set_trace()
-        output = [chromosome.label, evaluation_site, lod]
+    
+       # lod = np.nan
+    
+        output = [chromosome.label, chromosome.physical_map[markidx], vc.lod, vc.pvalue]
+        outputlist.append(output)
         print '\t'.join(str(x) for x in output)
+
+
+print 'Done!'
